01d34f83e08bd954994b97b0ab9c3fbc2bdcb7aa
Dylan Zakaharinivo <zakadylan2005@gmail.com>
2025-11-10 16:06:18 +0100

    Add authentication routes and middleware for protected task access

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const path = require('path');
 const taskRoutes = require('./src/routes/taskRoutes');
+const authRoutes = require('./src/routes/authRoutes');
+const auth = require('./src/middleware/auth');
 
 // créer l'application Express
 const app = express();
@@ -17,6 +19,12 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
+// Routes publiques (sans authentification)
+app.use('/api/auth', authRoutes);
+
+// Routes protégées (avec authentification)
+app.use('/api/tasks', auth, taskRoutes);
 
 // utiliser les routes pour les tâches
 app.use('/api/tasks', taskRoutes);
